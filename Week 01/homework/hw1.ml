f7c49dadefcca6b1646930b2f5aaa67ea79c5709
Ryan Sharif <rsharif@g.ucla.edu>
2016-01-10 08:04:58 -0800

    fixed equal_sets function

--- Week 01/homework/hw1.ml
+++ Week 01/homework/hw1.ml
@@ -1,10 +1,8 @@
 (* function for checking whether A is a subset of B  *)
-let rec subset a b =
-  match a with
+
+let rec subset a b = match a with
   | [] -> true
-  | hd_a :: tl_a -> 
-    match b with
-    | [] -> true
-    | hd_b :: tl_b -> if hd_a = hd_b then subset tl_a b else false
+  | hd :: tl ->
+     if (List.mem b hd) then subset tl b else false
 ;;
 
