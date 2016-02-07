1983f4e968fd372cd7acfe77427a275637a1c42c
Ryan Sharif <rsharif@g.ucla.edu>
2016-02-07 09:25:15 -0800

    fixed deadlock problem for BetterSafe

--- Week 05/files/BetterSafe.java
+++ Week 05/files/BetterSafe.java
@@ -1,31 +1,37 @@
 
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class BetterSafe implements State {
     private byte[] value;
     private byte maxval;
-    private final ReentrantReadWriteLock swapLock = new ReentrantReadWriteLock();
+    private final ReentrantLock swapLock;
 
-    BetterSafe(byte[] v) { value = v; maxval = 127; }
+    BetterSafe(byte[] v) {
+        value = v; maxval = 127;
+        swapLock = new ReentrantLock();
+    }
 
-    BetterSafe(byte[] v, byte m) { value = v; maxval = m; }
+    BetterSafe(byte[] v, byte m) {
+        value = v; maxval = m;
+        swapLock = new ReentrantLock();
+    }
 
 public int size() { return value.length; }
 
 public byte[] current() { return value; }
 
 public boolean swap(int i, int j) {
-    swapLock.writeLock().lock();
-    swapLock.readLock().lock();
+    swapLock.lock();
     
     if (value[i] <= 0 || value[j] >= maxval) {
+        swapLock.unlock();
+
         return false;
     }
     value[i]--;
     value[j]++;
 
-    swapLock.writeLock().unlock();
-    swapLock.readLock().unlock();
+    swapLock.unlock();
     
     return true;
                             }
