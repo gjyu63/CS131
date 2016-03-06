34b3ab5d00d3229481039e91a9dd03887f996f73
Ryan Sharif <rsharif@g.ucla.edu>
2016-03-05 23:26:45 -0800

    added beginning of WHATSAT

--- Week 09/server.py
+++ Week 09/server.py
@@ -1,15 +1,20 @@
 from twisted.internet import reactor, protocol
 from twisted.internet.defer import Deferred
-import sys
+import sys, re
+from time import time
+
+# Byte-compiled regular expression for GPS coordinates
+r = re.compile("([\+|-]\d+\.\d+)([\+|-]\d+\.\d+)")
 
 class AtServer( protocol.Protocol ):
     def __init__( self ):
         self.name = sys.argv[1]
-        self.d = Deferred()
-        self.d.addCallback( self.handleInput )
+        self.servers = {}
         
     def dataReceived( self, data ):
-        self.d.callback( data )
+        d = Deferred()
+        d.addCallback( self.handleInput )
+        d.callback( data )
 
     def handleInput(self, data):
         # process the data received
@@ -18,27 +23,96 @@ class AtServer( protocol.Protocol ):
                   zip( types, data.split(" ")) ]
         # figure out the command
         if( input[0] == "IAMAT" ):
-            self.dIAMAT = Deferred()
-            self.dIAMAT.addCallback(self.handleIAMAT)
-            self.dIAMAT.callback(input)
-            
-            self.transport.write( input[0] )
+            dIAMAT = Deferred()
+            dIAMAT.addCallback(self.handleIAMAT)
+            dIAMAT.callback(input)
 
-        elif( input[0] == "AT" ):
-            self.transport.write( "fix this" )
+        elif( input[0] == "WHATSAT" ):
+            dAT = Deferred()
+            dAT.addCallback(self.handleWHATSAT)
+            dAT.callback(input)
+            
+            self.transport.write( "fix this:\n" )
         else:
             self.transport.write( "?" )
-        # execute the command if it is valid
 
-        # add the correct callback
-
-        # fire the callback
-
-    def handleIAMAT(self, input):
-        t1 = input[3]
-        self.transport.write("You said the time was: %s" % t1)
+    def storeServerLocation(self, input, time, time_diff):
+        '''Store a server's location.
         
-    def calculate_time_difference(t1, t2):
+        arguments:
+        input[1] = client name
+        input[2] = location
+        input[3] = client time
+        time - server time
+        time_diff - server-client time difference
+
+        stores:
+        server name, time difference, client,
+        location, server-client time
+        '''
+        client = input[1]
+        location = input[2]
+
+        # parse the GPS location
+        m = r.match(location)
+        latitude = float(m.group(1))
+        longitude = float(m.group(2))
+
+        self.servers[client] = {
+            "server-name"     : self.name,
+            "time-difference" : time_diff,
+            "client-name"     : client,
+            "location"        : location,
+            "sc-time"         : time,
+            "latitude"        : latitude,
+            "longitude"       : longitude }
+            
+    def handleIAMAT(self, input):
+        client_time = input[3]
+        server_time = time()
+        time_diff = self.calculate_time_difference(
+            client_time, server_time)
+
+        server_name = self.name
+        client_server = input[1]
+        location = input[2]
+
+        self.storeServerLocation(input, server_time, time_diff)
+        
+        output_str = "AT %s %s %s %s\n" % ( server_name,
+                                             time_diff,
+                                             client_server,
+                                             time_diff )
+        
+        self.transport.write( "%s" % output_str )
+
+    def handleWHATSAT(self, input):
+        '''input will be a list of values:
+        input[0] = WHATSAT command
+        input[1] = client name
+        input[2] = information upper bound
+        input[3] = radius of the client
+
+        the server should respond with information, if
+        such a client has already been stored:
+
+        AT <server> <time difference> <client> <location>...
+        <time of server-client interaction> <GOOGLE info>
+        '''
+        client = input[1]
+        upperbound = input[2]
+        radius = input[3]
+        lookup = self.servers[client]
+        
+        self.transport.write(
+            "AT %s %s %s %s %s\n" % (
+                lookup["server-name"],
+                lookup["time-difference"],
+                lookup["client-name"],
+                lookup["location"],
+                lookup["sc-time"] ))
+                
+    def calculate_time_difference(self, t1, t2):
         time_diff = str(float(t2) - float(t1))
         if (float(time_diff) >= 0):
             time_diff = "+" + time_diff
